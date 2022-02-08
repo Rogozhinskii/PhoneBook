f91fe340c863bd38521a8345c8420e594f089b02
Rogozhinskii <rogozhinskii96@mail.ru>
2022-02-08 23:19:36 +0500

    Переименование контроллера аутентификации

---
+++ Services/PhoneBook.WebApiClient/ApiAuthentification.cs
@@ -0,0 +1,28 @@
+﻿using PhoneBook.Interfaces;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PhoneBook.WebApiClient
+{
+    public class ApiAuthentification : IAuthentificationService
+    {
+        public string AuthenticatedUserName => throw new System.NotImplementedException();
+
+        public string AuthenticatedUserRole => throw new System.NotImplementedException();
+
+        public Task<string> GetUserRoleAsync(string userName, CancellationToken cancel = default)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> Login(IUserLogin login, CancellationToken cancel = default)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> Logout(CancellationToken cancel = default)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
